213b2ebcdea9d6f2b5d9791e5f444210fe0e338e
evolveagencymarketing <alejandro@evolvea.net>
2025-09-02 17:55:28 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BuilderComponent, builder } from "@builder.io/react";
 
 // Connect your Builder.io account
-builder.init("YOUR_PUBLIC_API_KEY"); // replace with your actual Builder.io Public API Key
+builder.init("177ff67c0ebd4a44bbeeb92482f9379c"); // replace with your actual Builder.io Public API Key
 
 export default function App() {
   return (
